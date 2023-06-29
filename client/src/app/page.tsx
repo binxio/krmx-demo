9f0f24b1461cda43ca750f257da1ce0b53e82f0b
Simon Karman <simon.karman@xebia.com>
2023-06-29 22:20:37 +0200

    Added slightly improved UI

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,8 +2,10 @@
 
 export default function Home() {
   return <main>
-    <h1>My App</h1>
-    <MyApp />
+    <h1 className='border-b px-4 py-2'>My First Krmx Game!</h1>
+    <div className='px-4 py-2'>
+      <MyApp />
+    </div>
   </main>
 };
 import {KrmxProviderWithStore, useKrmx} from '@krmx/client';
@@ -26,26 +28,26 @@ function MyComponent() {
   const { isConnected, isLinked, link, rejectionReason, send, leave, users, username } = useKrmx();
   if (!isConnected) {
     // Your logic for when you're not connected to the server goes here
-    return <p>No connection to the server...</p>;
+    return <p className='text-red-800'>No connection to the server...</p>;
   }
   if (!isLinked) {
     // Your logic for linking your connection with a user goes here
     return (
-      <div>
-        <button onClick={() => link('simon')}>Join!</button>
-        {rejectionReason && <p>Rejected: {rejectionReason}</p>}
-      </div>
+      <>
+        <button className='border px-2 py-1 rounded' onClick={() => link('simon')}>Join as simon!</button>
+        {rejectionReason && <p className='text-red-800'>Rejected: {rejectionReason}</p>}
+      </>
     );
   }
   // Your logic for when you're ready to go goes here
   return (
-    <div>
-      <p>
+    <>
+      <p className='mb-1'>
         Welcome <strong>{username}</strong>!
       </p>
-      <button onClick={() => send({ type: 'custom/hello' })}>Send custom/hello</button>
-      <button onClick={leave}>Leave</button>
-      <h2>Users</h2>
+      <button className='border px-2 py-1 rounded' onClick={() => send({ type: 'custom/hello' })}>Send custom/hello</button>
+      <button className='border px-2 py-1 rounded' onClick={leave}>Leave</button>
+      <h2 className='mt-2 mb-1'>Users</h2>
       <ul>
         {Object.entries(users)
           .map(([otherUsername, { isLinked }]) => (
@@ -54,6 +56,6 @@ function MyComponent() {
             </li>)
           )}
       </ul>
-    </div>
+    </>
   );
 }
